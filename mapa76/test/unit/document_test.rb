afe600c8a7a74f8f073bd18eaf78edfc3c5f9976
malev <marcosvanetta@gmail.com>
2012-09-03 22:16:21 -0300

    Add document, person and blacklist models

--- mapa76/test/unit/document_test.rb
+++ mapa76/test/unit/document_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class DocumentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  context "Document" do
+    setup do
+    end
+    should "Validate presence and uniqueness of title" do
+      validate_presence_of(:title)
+      validate_uniqueness_of(:title)
+    end
+    should "generate CSV with all the people"
+    should "Export registers as CSV"
+  end
 end
