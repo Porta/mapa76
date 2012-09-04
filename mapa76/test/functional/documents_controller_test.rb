af3f72517c13bb0526e54523ee4c658d7a797c0f
malev <marcosvanetta@gmail.com>
2012-09-04 01:19:12 -0300

    Add documents controller with index and show

---
+++ mapa76/test/functional/documents_controller_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class DocumentsControllerTest < ActionController::TestCase
+  context "Documents list and show" do
+    setup do
+      @document = create :document
+    end
+
+    should "Should list document's name" do
+      get :index
+      assert_response :success
+      assert_template :index
+      assert_not_nil assigns(:documents)
+      assert_select 'li.title', @document.title
+    end
+
+    should "Show one document" do
+      get :show, id: @document.id
+      assert_response :success
+      assert_template :show
+      assert_not_nil assigns(:document)
+      assert_select 'h1.title', @document.title
+    end
+  end
+end
