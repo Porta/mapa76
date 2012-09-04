b91bb0c06d492e35a5f84e848ebdac28a6df53ab
malev <marcosvanetta@gmail.com>
2012-09-03 22:15:43 -0300

    Add empty home page

---
+++ mapa76/test/functional/welcome_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class WelcomeControllerTest < ActionController::TestCase
+  context "WelcomeController" do
+    should "Respond 200" do
+      assert_response 200
+    end
+  end
+end
